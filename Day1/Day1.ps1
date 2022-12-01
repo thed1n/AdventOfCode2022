28e576c4bb5c06fbeabd42c461c7fded0d2b34c2
thed1n <tomas.hedin@gmail.com>
2022-12-01 08:02:22 +0100

    day 1

--- Day1/Day1.ps1
+++ Day1/Day1.ps1
@@ -1,1 +1,16 @@
 $data = get-content .\Day1\Input1.txt
+
+$santa = @{}
+$i = 0
+
+foreach ($d in $data) {
+    if (!$d) {$i++}
+    $santa[$i]+=[int]$d
+}
+
+
+
+[pscustomobject]@{
+part1 = $santa.values|sort -Descending|select -first 1
+part2 = $santa.values|sort -Descending|select -first 3 | Measure-Object -sum | % sum
+}
